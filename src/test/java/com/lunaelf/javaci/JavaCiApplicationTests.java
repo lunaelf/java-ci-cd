fc4c9b70a53f34036ad6a429c8af6618945d44aa
Jiafei Peng <jiafeipeng92@gmail.com>
2024-11-25 20:42:53 +0800

    initial commit

---
+++ src/test/java/com/lunaelf/javaci/JavaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaelf.javaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
